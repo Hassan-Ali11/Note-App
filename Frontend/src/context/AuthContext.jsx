79277cba63e53086c10adcb2346c55c48d28bd43
Hassan-Ali11 <hassannazir1974@gmail.com>
2025-08-20 08:36:44 +0500

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,29 +1,3 @@
-// // src/context/AuthContext.js
-// import { createContext, useState, useContext } from "react";
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null); // initially no user
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// // Custom hook for easier usage
-// export const useAuth = () => useContext(AuthContext);
-
-
-
-
-
-
-
-
-
 import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
